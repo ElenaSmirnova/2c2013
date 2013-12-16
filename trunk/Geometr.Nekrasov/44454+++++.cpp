ef7fae89d78df3d6a794fb2f0d304c4090b07589
geometr.nekrasov@gmail.com <geometr.nekrasov@gmail.com@f0e1bf5e-d2b3-ee41-f0f5-a9bffd97f7fd>
2013-12-16 07:21:23 +0000

    Task 4.2-4

--- trunk/Geometr.Nekrasov/44454+++++.cpp
+++ trunk/Geometr.Nekrasov/44454+++++.cpp
@@ -4,7 +4,7 @@
 #define u 100
 
 
-//using namespace std;
+using namespace std;
 int s[u][u];
 int q[u];
 
@@ -29,8 +29,10 @@ class Vector(){
 
         ~Vector(void);
 
-        Vector(const Vector &vec) { size = vec.size;  
-        for(i=0;i<size;i++){x[i]=vec.x[i];}  }
+
+        Vector(const Vector &vec) { size = vec.size;
+        for(i=0;i<size;i++){x[i]=vec.x[i];}  }
+
 
         int size(){
             return size;
@@ -77,13 +79,11 @@ class Vector(){
             return v;
             }
 
-        Vector operator/(Vector vec) const{
+        int operator/(Vector vec) const{
 
-            for(i=0;i<u;i++){q[i]=0;}
-
-            Vector v(size,q[u]);
-            for(i=0;i<size;i++){v.x[i]=x[i]*vec.x[i]};
-            return v;
+            int scal=0;
+            for(i=0;i<size;i++){scal = scal + x[i]*vec.x[i]};
+            return scal;
             }
 
         Vector operator%(Matrix mat) const{
@@ -118,6 +118,11 @@ class Matrix(){
             return length;
             }
 
+        int width(){
+            return width;
+            }
+
+
         Matrix(int length,int width,int a[u][u])
         {
             SetMatrix(length,width,a[u][u]);
@@ -125,15 +130,10 @@ class Matrix(){
 
         ~Matrix(void);
 
-        Matrix(const Matrix &vec) { length = vec.length;
-        width=vec.width;  
+        Matrix(const Matrix &vec) { length = vec.length;
+        width=vec.width;
         for(i=0;i<width;i++){
-                for(j=0;j<length;j++){a[i][j]=vec.a[i][j]};}  }
-
-        int width(){
-            return width;
-            }
-
+                for(j=0;j<length;j++){a[i][j]=vec.a[i][j]};}  }
 
 
         void SetMatrix(int length_1,int width_1,int a_1[u][u])
@@ -200,7 +200,6 @@ class Matrix(){
             for(i=o;i<u;i++){for(j=o;j<u;j++){s[i][j]=0;}}
 
             Matrix M(width, length,s[u][u]);
-            //M=SetMatrixSet(width, length, s[u][u])
             for(i=0;i<length;i++){
             for(j=0;j<width;j++){
             M.a[i][j]=a[j][i];
@@ -216,10 +215,50 @@ class Matrix(){
         Matrix inverse()
         {
 
+        int i,j,k,l, a, b, sum = 0;
+        Matrix M(length, length,s[u][u]);
+
+    for (i = 0; i < size; i ++ )
+    {
+
+        for (j = 0; j < size; j ++ )
+            M.a[i][j] = 0;
+            M.a[i][i] = 1;
+    }
+    for ( int i = 0; i < size; i ++ )
+    {
+        a = a[i][i];
+        for ( int j = i + 1; j < size; j ++ )
+        {
+            b = a[j][i];
+            for ( int k = 0; k < size; k ++ )
+            {
+                a[j][k] = a[i][k] * b - a[j][k] * a;
+                M.a[j][k]= M.a[i][k] * b - M.a[j][k] * a;
+            }
         }
+    }
+    for (i = 0; i < size; i ++ )
+    {
+        for (j = size - 1; j >= 0; j -- )
+        {
+            for (k = size - 1; k > j; k -- )
+                sum += a[j][k] * M.a[k][i];
+                if ( a[j][j] == 0 )
+                {
+                    for (l = 0; l < size; l ++ )
+                        delete [] M.a[l];
+                        delete [] M;
+                }
+                M.a[j][i] = ( M.a[j][i] - sum ) / a[j][j];
+        }
+    }
+
+            return M;
+    }
 }
 
-Vector Hill(string key, string String)
+string Hill(string key, string String)
 {
     int l,h,l_1,code;
     int a[u][u];
@@ -264,7 +303,7 @@ Vector Hill(string key, string String)
     return String;
 }
 
-Vector DeHill(string key, string String)
+string DeHill(string key, string String)
 {
         int l,h,l_1,code;
     int a[u][u];
@@ -310,4 +349,33 @@ Vector DeHill(string key, string String)
       }
 
     return String;
+}
+
+
+int main(){
+string String, key, Hilll;
+int Answer;
+
+
+    std::cout << "Enter message" ;
+    std::cin  >> String;
+
+
+    std::cout << "Enter key" ;
+    std::cin  >> key;
+
+
+    std::cout << "Enter crypt - 0 or decryp - 1" ;
+    std::cin  >> Answer;
+
+
+        if(Answer==0){
+        Hilll=Hill(key, String);
+        }
+        else{
+        Hilll=DeHill(key, String);
+        }
+
+        std::cin  >> Hilll;
+
 }
