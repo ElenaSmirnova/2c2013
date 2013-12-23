5402249a4c9730e99353f413e009986f0db59f67
mike239x@gmail.com <mike239x@gmail.com@f0e1bf5e-d2b3-ee41-f0f5-a9bffd97f7fd>
2013-12-23 20:19:53 +0000

    Geometry module, contains classes Circle and Line and function for finding thier intersection.

---
+++ trunk/mike239x/geom.h
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "vector.h"
+#include <math.h>
+
+using namespace std;
+
+class Line {
+	// y = ax + b
+	private:
+	double a, b;
+	public:
+	double getA() const{
+		return a;}
+	double getB() const{
+		return b;}
+	void setA(double a){
+		this->a = a;}
+	void setB(double b){
+		this->b = b;}
+	Line (double a, double b) {
+		setA(a); 
+		setB(b);
+		print();
+	}
+	void print() {
+		printf("y = %gx+%g\n", a, b);
+	}
+};
+
+class Circle {
+	// (x-cx)^2 + (y-cy)^2 = R^2
+	private:
+	double R;
+	double cx, cy;
+	public:
+	double getR() const{
+		return R;}
+	double getCX() const{
+		return cx;}
+	double getCY() const{
+		return cy;}
+	void setR(double R){
+		this->R = R;}
+	void setCX(double cx){
+		this->cx = cx;}
+	void setCY(double cy){
+		this->cy = cy;}
+	Circle (double R, double cx, double cy) {
+		setR(R); 
+		setCX(cx);
+		setCY(cy);
+		print();
+	}
+	void print() {
+		printf("(x-%g)^2 + (y-%g)^2 = %g^2\n", cx, cy , R);
+	}
+};
+
+double* intersection(Line l, Circle c) {
+	double A = (l.getA()*l.getA()+1);
+	double B = -2*(c.getCX() - (l.getB()-c.getCY())*l.getA());
+	double K = c.getCX()*c.getCX() + (l.getB() - c.getCY())*(l.getB() - c.getCY()) - c.getR()*c.getR();
+	double D = B*B - 4*A*K;
+	printf("%gXX + %gX + %g = 0\n",A,B,K,D);
+	if (D < 0) {
+		return 0;
+	}
+	D = sqrt(D);
+	double x1 = (-B+D)/(2*A);
+	double x2 = (-B-D)/(2*A);
+	printf("x1 = %g, x2 = %g\n",x1,x2);
+	double y1 = l.getA() * x1 + l.getB();
+	double y2 = l.getA() * x2 + l.getB();
+	double* result = new double[4];
+	result[0] = x1;
+	result[1] = y1;
+	result[2] = x2;
+	result[3] = y2;
+	return result;
+};
